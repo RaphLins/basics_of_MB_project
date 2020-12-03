117d98bd72619ad37f726b283d61b3ce6cf83aca
RaphLins <raphael.secondaire@gmail.com>
2020-12-03 12:37:02 +0100

    vision code clean up + option to take picture directly from camera

---
+++ src/utils/vision_utils.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+def find_rectangle(threshold_image, minimum_area, arclen, plot=True):
+    # Detecting shapes in image by selecting region with same colors or intensity.
+    contours,_=cv2.findContours(threshold_image, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    board = []
+
+    # Searching through every region selected to find the required polygon.
+    for cnt in contours :
+        area = cv2.contourArea(cnt)
+
+        # Shortlisting the regions based on there area.
+        if area > minimum_area:
+            approx = cv2.approxPolyDP(cnt,
+                                      arclen
+                                      * cv2.arcLength(cnt, True), True)
+            if len(approx) == 4:
+                board = approx
+
+    if not board.any():
+        print("Sheet not found")
+        return
+    else:
+        if plot:
+            n= len(board)
+            for i in range(n):
+                vertice1 = board[i][0]
+                vertice2 = board[(i+1)%n][0]
+                plt.scatter(vertice1[0], vertice1[1], c='r')
+                plt.plot((vertice1[0], vertice2[0]), (vertice1[1], vertice2[1]), 'r')
+
+    top_left_idx = None
+    for i in range(len(board)):
+        vertice1 = board[i][0]
+        if vertice1[0] < threshold_image.shape[0] and vertice1[1] < threshold_image.shape[1]:
+            top_left_idx = i
+            break
+    new_board = []
+    for i in range(len(board)):
+        idx = (top_left_idx + i) % 4
+        new_board.append(board[idx][0])
+
+    return new_board
+
+def remove_black(img, max_black):
+    black_min = np.array([0, 0, 0])
+    black_max = np.array([max_black, max_black, max_black])
+
+    mask = cv2.inRange(img, black_min, black_max)
+
+    img_no_black = img.copy()
+    img_no_black[mask > 0] = (255, 255, 255)
+    return img_no_black
